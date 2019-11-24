22f324f0b4acbadcdbb0ce2191550c9bba4e368a
Chot <chotxhm@gmail.com>
2019-11-24 03:02:32 +0200

    Updates

---
+++ precache-manifest.43860477a7e8c51cafb0ee70d9cadd40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96fd438f657f910bce5163b0a00c924f",
+    "url": "/webapptest/index.html"
+  },
+  {
+    "revision": "61f9cb502c70875a0678",
+    "url": "/webapptest/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "0375bf54655f3b31edc9",
+    "url": "/webapptest/static/css/main.c11bebf3.chunk.css"
+  },
+  {
+    "revision": "61f9cb502c70875a0678",
+    "url": "/webapptest/static/js/2.eaf6b19d.chunk.js"
+  },
+  {
+    "revision": "0375bf54655f3b31edc9",
+    "url": "/webapptest/static/js/main.9fbf8499.chunk.js"
+  },
+  {
+    "revision": "0a2b417891a87cf53302",
+    "url": "/webapptest/static/js/runtime-main.263833f8.js"
+  },
+  {
+    "revision": "7cc29abbdd9361a533477f0c95115a86",
+    "url": "/webapptest/static/media/logo.7cc29abb.png"
+  }
+]);
